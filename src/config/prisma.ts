7ed9e2eed36d9a551e4e3316868b2e8e74fa7d31
IvanOrsh <ksironov@mail.ru>
2023-06-22 02:19:45 +0300

    add prisma config

---
+++ src/config/prisma.ts
@@ -0,0 +1,15 @@
+import { PrismaClient } from '@prisma/client';
+import config from './config';
+
+declare global {
+  // eslint-disable-next-line no-var
+  var prisma: PrismaClient | undefined;
+}
+
+const prismaClient: PrismaClient = new PrismaClient();
+
+if (config.node_env !== 'production') {
+  global.prisma = prismaClient;
+}
+
+export default prismaClient;
